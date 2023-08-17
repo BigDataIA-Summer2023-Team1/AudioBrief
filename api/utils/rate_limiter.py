1dad1538659657c39b063d053f11be3ae60880be
Shiva Sai Charan Ruthala <113181307+shivasaicharanruthala@users.noreply.github.com>
2023-08-17 11:45:22 -0400

    Update rate_limiter.py

--- api/utils/rate_limiter.py
+++ api/utils/rate_limiter.py
@@ -1,7 +1,6 @@
 import os
 import redis
 import datetime
-# from cloud_logger import logger
 from fastapi import HTTPException
 from google.cloud import logging as gcl
 
@@ -17,51 +16,39 @@ def get_redis_pool():
 
 
 # Rate limiter dependency
-async def rate_limiter(user_id: str, plan: str):
+async def rate_limiter(user_id: str, plan: str, log):
     # log = logger()
-    # resource = gcl.Resource(type='global', labels={"type": "application"})
-    try:
-        client = get_redis_pool()
+    resource = gcl.Resource(type='global', labels={"type": "application"})
 
-        current_count = client.get(user_id)
-        if current_count is None:
-            client.setex(user_id, 86400, 1)  # Set the count to 1 and expire after 24 hours
-        elif plan == "basic" and int(current_count) >= 100:
-            # payload = {
-            #     "endpoint": "",
-            #     "timestamp": datetime.datetime.now(),
-            #     "user": user_id,
-            #     "status": 429,
-            #     "execution_time": "",
-            #     "msg": "Request limit exceeded for a basic plan"
-            # }
-            #
-            # log.log_text(str(payload), resource=resource, severity="ERROR")
-            raise HTTPException(status_code=429, detail="Request limit exceeded")
-        elif plan == "premium" and int(current_count) >= 20:
-            # payload = {
-            #     "endpoint": "",
-            #     "timestamp": datetime.datetime.now(),
-            #     "user": user_id,
-            #     "status": 429,
-            #     "execution_time": "",
-            #     "msg": "Request limit exceeded for a premium plan"
-            # }
-            #
-            # log.log_text(str(payload), resource=resource, severity="ERROR")
-            raise HTTPException(status_code=429, detail="Request limit exceeded")
-        else:
-            client.incr(user_id)
-    except Exception as exp:
-        # payload = {
-        #     "endpoint": "",
-        #     "timestamp": datetime.datetime.now(),
-        #     "user": user_id,
-        #     "status": 429,
-        #     "execution_time": "",
-        #     "msg": str(exp)
-        # }
-        #
-        # log.log_text(str(payload), resource=resource, severity="ERROR")
-        print(str(exp))
+    client = get_redis_pool()
 
+    current_count = client.get(user_id)
+    if current_count is None:
+        client.setex(user_id, 86400, 1)  # Set the count to 1 and expire after 24 hours
+        print("here 8")
+    elif plan == "basic" and int(current_count) >= 10:
+        payload = {
+            "endpoint": "",
+            "timestamp": datetime.datetime.now(),
+            "user": user_id,
+            "status": 429,
+            "execution_time": "",
+            "msg": "Request limit exceeded for a basic plan"
+        }
+
+        log.log_text(str(payload), resource=resource, severity="ERROR")
+        raise HTTPException(status_code=429, detail="Request limit exceeded")
+    elif plan == "premium" and int(current_count) >= 20:
+        payload = {
+            "endpoint": "",
+            "timestamp": datetime.datetime.now(),
+            "user": user_id,
+            "status": 429,
+            "execution_time": "",
+            "msg": "Request limit exceeded for a premium plan"
+        }
+
+        log.log_text(str(payload), resource=resource, severity="ERROR")
+        raise HTTPException(status_code=429, detail="Request limit exceeded")
+    else:
+        client.incr(user_id)
